b24960ebb156a5fd90127b7904d475ec1abb4b0b
Avinashg137 <avinashg.itpath@gmail.com>
2022-03-24 16:30:19 -0700

    Initial Commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by MAC OS on 23/03/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+     static let registerSegue = "RegisterToChat"
+     static let loginSegue =  "LoginToChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    
+
+struct BrandColors {
+    static let purple = "BrandPurple"
+    static let lightpurple = "BrandLightPurple"
+    static let blue = "BrandBlue"
+    static let lightBlue = "BrandLightBlue"
+}
+    struct FStore {
+        static let collectionName = "Massages"
+        static let senderField = "Sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+
+}
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by MAC OS on 23/03/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+
+    let sender : String  // 1@2.com
+    let body : String
+}
+
